07330871d92ee748a1d6cfd7d1c0c0762a48cf9f
cdbernal01-oss <cdbernal01@gmail.com>
2025-11-07 15:23:02 -0500

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className="text-center py-2" fontColor='black'>
-            <p>HouseStage &copy; Todos los derechos reservados {currentYear} Soporte y ayuda En caso de necesitar ayuda o soporte con la pagina por favor ponerse en contacto a través del correo cdbernal@housetage.com o a la línea +57 3152005881 en el horario de lunes a viernes de 8 am a 5 pm
+            <p>HouseStage &copy; Todos los derechos reservados {currentYear} Desarrollador, Diseñador, Creador, Analista QA de la autoria de Cristian David Bernal Rubiano, Soporte y ayuda En caso de necesitar ayuda o soporte con la pagina por favor ponerse en contacto a través del correo cdbernal@housetage.com o a la línea +57 3152005881 en el horario de lunes a viernes de 8 am a 5 pm
 </p>
           </Col>
         </Row>
